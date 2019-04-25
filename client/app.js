c840f2a0746a962199683783aa2b597a0f431383
Harrison Cole <harrison.d.cole@gmail.com>
2019-04-25 12:56:59 -0400

    remove redux and render main component

--- client/app.js
+++ client/app.js
@@ -1,13 +1,12 @@
 import React from 'react'
-
-import {Navbar} from './components'
-import Routes from './routes'
+import Main from './components/Main'
+// import {Navbar} from './components'
+// import Routes from './routes'
 
 const App = () => {
   return (
     <div>
-      <Navbar />
-      <Routes />
+      <Main />
     </div>
   )
 }
