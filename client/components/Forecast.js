926b0da8296c0769d3ebdc0614147ad15584dd35
Harrison Cole <harrison.d.cole@gmail.com>
2019-04-26 17:34:09 -0400

    forecast blurb

--- client/components/Forecast.js
+++ client/components/Forecast.js
@@ -2,19 +2,31 @@ import React from 'react'
 import {unixToTime} from '../utils'
 
 const Forecast = ({forecast}) => {
-  const high = forecast.temperatureHigh
   const highTime = unixToTime(forecast.temperatureHighTime)
-  const low = forecast.temperatureLow
   const lowTime = unixToTime(forecast.temperatureLowTime)
+  const sunrise = unixToTime(forecast.sunriseTime)
+  const sunset = unixToTime(forecast.sunsetTime)
+  const precipProb = forecast.precipProbability * 100
+  const precipType = forecast.precipType
+  const high = forecast.temperatureHigh
+  const low = forecast.temperatureLow
+  const humidity = forecast.humidity * 100
+  const uvIndex = forecast.uvIndex
+  const cloudCover = forecast.cloudCover * 100
 
   return (
     <div>
-      <h1>
-        Low of {low} at {lowTime}
-      </h1>
-      <h1>
-        High of {high} at {highTime}
-      </h1>
+      {low === 'NaN' ? (
+        <p>loading...</p>
+      ) : (
+        <p>
+          Today's forecast has a low of {low} at {lowTime}, a high of {high} at{' '}
+          {highTime}. Sunrise at {sunrise} and sunset at {sunset}. There's a{' '}
+          {precipProb}% chance of {precipType ? precipType : 'precipitation'}.
+          Humidity will be {humidity}% and there's a UV Index of {uvIndex}.
+          There will be an average of {cloudCover}% cloud cover.
+        </p>
+      )}
     </div>
   )
 }
--- client/utils.js
+++ client/utils.js
@@ -1,4 +1,5 @@
 export function unixToTime(unixStr) {
+  console.log('unix string:', unixStr)
   const date = new Date(unixStr * 1000)
   const hours = date.getHours()
   const minutes = '0' + date.getMinutes()
