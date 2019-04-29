805825c6e3e78fa2796d42cad0d2d1ad777e1350
Harrison Cole <harrison.d.cole@gmail.com>
2019-04-28 21:27:23 -0400

    updated clothing logic and styling

--- client/components/Wardrobe.js
+++ client/components/Wardrobe.js
@@ -17,23 +17,36 @@ const Wardrobe = ({
   let umbrella = precipProb > 15 && precipType !== 'snow'
   let jacket = false
   let sungear = uvIndex > 5.9
+  let top = 'tee shirt or dress or something light'
+  let bottom =
+    "shorts baby! or just skip the top and throw on a dress if that's your thing"
 
   if (high < 32 - offset) {
     jacket = 'parka'
+    bottom = 'warm pants'
+    top = 'sweater or hoodie'
     umbrella = false
   } else if (high < 40 - offset) {
     jacket = 'winter coat'
+    bottom = 'pants'
+    top = 'long-sleeved shirt or top'
   } else if (high < 50 - offset) {
     jacket = 'midweight jacket'
+    bottom = 'pants'
+    top = 'long-sleeved shirt or top'
   } else if (high < 60 - offset) {
     jacket = 'light jacket'
+    bottom = 'pants'
+    top = 'short sleeved top with a sweater'
   } else if (high < 65 - offset) {
     jacket = 'sweater or very light jacket'
+    bottom = 'pants'
+    top = 'short sleeved top'
   }
 
   return (
     <div className="clothing-recs">
-      <h3> Here are my thoughts on clothes: </h3>
+      <h3> Here's what to wear: </h3>
       <ul>
         {umbrella && <li> Bring an umbrella! </li>}
         {jacket ? (
@@ -41,6 +54,9 @@ const Wardrobe = ({
         ) : (
           <li> No need for a jacket today! </li>
         )}
+        <li>
+          I suggest a {top} and {bottom}.
+        </li>
         {sungear && (
           <li>
             {' '}
